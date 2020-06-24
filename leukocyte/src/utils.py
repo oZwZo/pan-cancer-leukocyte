2cedc25387ee9fb435e22fce21efa812e82a5274
oZwZo <zhengwzh5@hotmail.com>
2020-06-24 10:48:53 +0800

    can specify cancer type

--- leukocyte/src/utils.py
+++ leukocyte/src/utils.py
@@ -8,18 +8,22 @@ from sklearn.preprocessing import scale
 #######    read    ########
 ##########################
 
-def read_leukocyte(data_path,separate=False):
+def read_leukocyte(data_path,separate=False,csv=None):
     """
     read leukocyte rate, return the ALL.csv by default,
     when set `separate=True` will return a dict containing 10 kinds cancer
     """
     if separate:
-        pds= {}
-        for csv in os.listdir(data_path+'/leukocyte_ratio'):
-            if csv == 'ALL.csv':
-                continue              # skip ALL.csv
-            fn = csv.split('.')[0]
-            pds[fn.upper()] = pd.read_csv(os.path.join(data_path,'leukocyte_ratio',csv))
+        if csv is None:
+            pds= {}
+            for csv in os.listdir(data_path+'/leukocyte_ratio'):
+                if csv == 'ALL.csv':
+                    continue              # skip ALL.csv
+                fn = csv.split('.')[0]
+                pds[fn.upper()] = pd.read_csv(os.path.join(data_path,'leukocyte_ratio',csv))
+        else:
+            assert(csv in os.listdir(data_path+'/leukocyte_ratio'))
+            pds = pd.read_csv(os.path.join(data_path,'leukocyte_ratio',csv))
     else:
         pds = pd.read_csv(os.path.join(data_path,'leukocyte_ratio','ALL.csv'))
     
