2de410be3c04c6e0d6622f6c4e7a89071bab84a6
zhengwzh5@hotmail.com <zhengwzh5@hotmail.com>
2020-06-24 10:03:51 +0800

    add more path and detect existence of path

--- leukocyte/src/PATH.py
+++ leukocyte/src/PATH.py
@@ -3,12 +3,15 @@ import os,sys
 global data_path
 global top_path
 global tsne_path
+global report_path
 
 data_path='/home/ZwZ/database/BRACA'
 top_path = '/home/ZwZ/script/HER2_prediction'
-tsne_path ='/home/ZwZ/script/HER2_prediction/leukocyte/t_sne'
+tsne_path = '/home/ZwZ/script/HER2_prediction/leukocyte/t_sne'
+report_path = '/home/ZwZ/script/HER2_prediction/leukocyte/cluster_report'
 
 sys.path.append(top_path)
+
 
 for path in [data_path,top_path,tsne_path]:
     if not os.path.exists(path):
